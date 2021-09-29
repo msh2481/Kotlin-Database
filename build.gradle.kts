d6c459d3059fac7c62213126d6fcd4aa885ac2e4
msh2481 <msh2481@ya.ru>
2021-09-29 18:57:51 +0300

    Attempt #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
     application
 }
 
@@ -13,6 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0-RC")
 }
 
 tasks.getByName<Test>("test") {
@@ -21,4 +23,15 @@ tasks.getByName<Test>("test") {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.WARN }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,62 @@
+import java.io.IOError
+import java.io.RandomAccessFile
+import java.security.MessageDigest
+import kotlin.reflect.KProperty
+import kotlin.text.Charsets.UTF_8
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.decodeFromString
+
+/**
+ * Converts array of bytes to a hex string
+ * @receiver array of bytes
+ * @return string where each byte is encoded by 2 chars
+ *
+ * Usage:
+ * assertEquals("010c13ac", byteArrayOf(1, 12, 19, -84).toHex())
+ */
+@ExperimentalUnsignedTypes
+fun ByteArray.toHex(): String = asUByteArray().joinToString("") { it.toString(radix = 16).padStart(2, '0') }
+
+/**
+ * Returns SHA-256 hash for a given string as an array of 32 bytes
+ * @param[str] string for which we want to get hash
+ *
+ * Usage:
+ * assertEquals("315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3", stringHash("Hello, world!").toHex())
+ */
+fun stringHash(str: String): ByteArray = MessageDigest.getInstance("SHA-256").digest(str.toByteArray(UTF_8))
+
+class Database(filename: String) {
+    val TEXT_BLOCK_SIZE = 32
+    val MAX_NODE_SIZE = 64
+    val file = RandomAccessFile(filename, "rw")
+
+    inner class NodePointer(val offset: Int) {
+        inline fun <reified NodeType> getValue(node: NodeType, property: KProperty<*>) : NodeType {
+            val buffer = ByteArray(MAX_NODE_SIZE)
+            try {
+                file.readFully(buffer, offset, MAX_NODE_SIZE)
+            } catch (e: IOError) {
+                println(e)
+                println("Failed to read bytes at [$offset; $offset + $MAX_NODE_SIZE) ")
+            }
+            return Json.decodeFromString(buffer.toString())
+
+        }
+
+        fun setValue(node: Any, property: KProperty<*>) : Unit {
+            val buffer = Json.encodeToString(node).toByteArray().copyOf(MAX_NODE_SIZE)
+            file.write(buffer, offset, MAX_NODE_SIZE)
+        }
+    }
+
+    inner class ListNode(val offset: Int) {
+        var nextPtr: NodePointer? = null
+        fun next
+    }
+}
+
 fun main(args: Array<String>) {
-    TODO()
 }
